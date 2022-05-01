ab82cab04f3096a81a4de6a193107f2e4ecb0cf4
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-01 15:51:12 -0400

    Add new restaurant component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Restaurant />
   </div>
 </template>
---
+++ components/Restaurant.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Top restaurants in Raleigh NC</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
