60e3d3f1cc54d539d0de0e49adf8a4540d5489b8
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-02 23:26:22 -0400

    Added reference to bootstrap stylesheet

--- components/Restaurant.vue
+++ components/Restaurant.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Top restaurants in Raleigh NC</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+// Just as the name of the file states, here is where we are going to configure my NUXT application
 export default defineNuxtConfig({
-
+    // For now all I want to do is add a LINK tag to all of my components
+    // (This will  be the way I import bootstrap into my app)
+    meta: {
+        link: [
+            {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            }
+        ]
+    }
 })
