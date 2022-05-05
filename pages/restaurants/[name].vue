b8bce608935c1fa18be2741472efd0b3e6c170bd
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-04 23:40:17 -0400

    Add functionality to pull data from the url parameter inside our dynamicly routed component

--- pages/restaurants/[name].vue
+++ pages/restaurants/[name].vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+import restaurants from "@/data.json"
+
+// Inside of NUXT we have composables
+// Composables are places where we can store global state
+// They usually start with the word USE
+const route = useRoute();
+const name = route.params.name;
+const restaurant = restaurants.find(r => r.name === name);
+</script>
+
 <template>
   <div>
-      TESTING DYNAMIC ROUTING USING Nuxt
+      {{ restaurant }}
   </div>
 </template>
-Lagarza
