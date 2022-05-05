3b32bc7a4443de7a00ee48c0057edf59eaa2f660
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-04 23:42:55 -0400

    Start displaying the info stored in my dummy data

--- pages/restaurants/[name].vue
+++ pages/restaurants/[name].vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
-import restaurants from "@/data.json"
+import restaurants from "@/data.json";
 
 // Inside of NUXT we have composables
 // Composables are places where we can store global state
 // They usually start with the word USE
 const route = useRoute();
 const name = route.params.name;
-const restaurant = restaurants.find(r => r.name === name);
+const restaurant = restaurants.find((r) => r.name === name);
 </script>
 
 <template>
-  <div>
-      {{ restaurant }}
+  <div class="restaurant-container">
+    <div class="image-container">
+      <img :src="restaurant.imageUrl" alt="" />
+    </div>
+    <div class="info-container">
+      <h1>{{ restaurant.name }}</h1>
+      <div class="stats-container">
+        <h5>Revenue (in billions)</h5>
+        <p>${{ restaurant.revenue }}</p>
+      </div>
+      <div class="stats-container">
+        <h5>Number of Stores</h5>
+        <p>{{ restaurant.numberOfStores }}</p>
+      </div>
+      <p class="content">{{ restaurant.content }}</p>
+    </div>
   </div>
 </template>
