2d44d5584db418e8412913a7b20f931a940b460b
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-05 23:25:49 -0400

    Handle case in which url path is not found. Custom 404

---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <Nav />
+  <div class="container">
+    <h1>PAGE NOT FOUND</h1>
+    <a href="/restaurants"> Go BACK </a>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
