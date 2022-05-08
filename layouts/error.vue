b711f4accc59577e44163544b08fd3acdc89c007
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-07 22:21:49 -0400

    Created new error layout

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Some Header</h1>
+      <div>
+        <img
+          src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
--- pages/404.vue
+++ pages/404.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="container">
     <h1>PAGE NOT FOUND</h1>
-    <a href="/restaurants"> Go BACK </a>
+
+    <div>
+      <img
+        src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+        alt=""
+      />
+    </div>
+
+    <NuxtLink to="/"> Go Back</NuxtLink>
   </div>
 </template>
 
@@ -9,5 +17,8 @@
 .container {
   text-align: center;
   margin-top: 5rem;
+}
+img {
+  width: 10rem;
 }
 </style>
