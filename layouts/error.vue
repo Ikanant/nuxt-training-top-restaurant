673e3108cc03888c960fc5203111d6b7d7fd7413
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-07 22:25:15 -0400

    Use layout with slit for redirectEl ... add styles around pages to make the site look better

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <div class="container">
-      <h1>Some Header</h1>
-      <div>
-        <img
-          src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
-          alt=""
-        />
-      </div>
+    <slot name="header"></slot>
+    <div>
+      <img
+        src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+        alt=""
+      />
     </div>
+    <slot name="redirectEl" />
   </div>
 </template>
+
+<style scoped>
+img {
+  width: 10rem;
+}
+</style>
